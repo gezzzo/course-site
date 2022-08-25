26ad3a6709a3cf36b3466f2d34abb104bd3a359b
gezzzo <gezzzo30@gmail.com>
2022-08-25 21:10:23 +0200

    first commit

---
+++ js/home.js
@@ -0,0 +1,43 @@
+// nav
+const btn = document.getElementById("nav_toggle");
+const links = document.querySelector(".nav_links");
+
+// toggle links
+btn.addEventListener("click", () => {
+    links.classList.toggle("show_links");
+});
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2023 20:29:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("demo").innerHTML =
+        hours + "h " + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+
+// plus
+const plus = document.getElementById("plus");
+const plusSection = document.getElementById("plusSection");
+
+plus.onclick = () => {
+    plusSection.classList.toggle("d-none");
+};
